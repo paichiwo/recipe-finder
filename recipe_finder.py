b028eef586f48920f0e79b2e9bcce4140feb2035
paichiwo <lzerucha@gmail.com>
2023-06-03 22:06:33 +0100

    Code basic functionality of an API call to the 'spoonacular.com'.

---
+++ recipe_finder.py
@@ -0,0 +1,98 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_api_key():
+    """ Get the api key from api_key.txt file."""
+    with open('api_key.txt') as api_file:
+        api_key = api_file.read()
+    return api_key
+
+
+def search_recipes(search_term, api_key):
+    """Search for recipes using keyword."""
+
+    recipes = []
+    url = 'https://api.spoonacular.com/recipes/complexSearch'
+    params = {
+        'apiKey': api_key,
+        'query': search_term,
+        'number': 10
+    }
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        recipe_results = response.json()['results']
+
+        for recipe in recipe_results:
+            recipe_id = recipe['id']
+            recipe_title = recipe['title']
+            recipe_image = recipe['image']
+            recipes.append((recipe_id, recipe_title, recipe_image))
+    print(recipes)
+    return recipes
+
+
+def get_recipe_information(recipe_id, api_key):
+    """Fetch the detailed recipe information."""
+
+    recipe_information = []
+    url = f'https://api.spoonacular.com/recipes/{recipe_id}/information'
+    params = {
+        'apiKey': api_key,
+        'includeNutrition': True  # Set to True if you want nutrition information included
+    }
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        recipe_details = response.json()
+
+        recipe_title = recipe_details['title']
+        recipe_instructions = recipe_details['instructions']
+
+        soup = BeautifulSoup(recipe_instructions, 'html.parser')
+        recipe_instructions_text = soup.get_text()
+
+        recipe_information.append((recipe_title, recipe_instructions_text))
+    else:
+        print("Error: Unable to fetch recipe details.")
+    print(recipe_information)
+    return recipe_information
+
+
+def get_recipe_ingredients(recipe_id, api_key):
+    """Fetch the ingredients for a given recipe ID."""
+
+    ingredients = []
+    url = f'https://api.spoonacular.com/recipes/{recipe_id}/ingredientWidget.json'
+    params = {
+        'apiKey': api_key,
+        'metric': 'true'  # Added metric parameter
+    }
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        ingredient_data = response.json()
+
+        for ingredient in ingredient_data['ingredients']:
+            amount = ingredient['amount']['metric']['value']
+            unit = ingredient['amount']['metric']['unit']
+            name = ingredient['name']
+            ingredients.append((name, amount, unit))
+        print(ingredients)
+        return ingredients
+    else:
+        return []
+
+
+def main():
+    key = get_api_key()
+    search_term = input("Search: ")
+    search_recipes(search_term, key)
+    recipe_id = input("Enter id: ")
+    get_recipe_ingredients(recipe_id, key)
+    get_recipe_information(recipe_id, key)
+
+
+if __name__ == "__main__":
+    main()
